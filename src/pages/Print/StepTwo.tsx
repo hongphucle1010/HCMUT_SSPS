40bfe08b7fafad7e96601849339090e8d6f586f3
Le Ngoc Vinh <vinh.lengoc@hcmut.edu.vn>
2024-10-16 13:09:37 +0700

    Step 3 added LTK image

--- src/pages/Print/StepTwo.tsx
+++ src/pages/Print/StepTwo.tsx
@@ -3,6 +3,7 @@ import styles from './StepTwo.module.scss'
 import { FaRegFilePdf } from 'react-icons/fa6'
 import { Link } from 'react-router-dom'
 import { LuArrowLeftCircle } from 'react-icons/lu'
+import img_map1 from '../../assets/map1.png'
 
 const Step2: React.FC = () => {
   const [nPage, setNPage] = useState(0)
@@ -11,12 +12,18 @@ const Step2: React.FC = () => {
 
   const [printSz, setPrintSz] = useState('none')
 
+  const [printLocation, setPrintLocation] = useState('none')
+
   const handleChange = (e: React.FormEvent<HTMLSelectElement>) => {
     setColor(e.currentTarget.value)
   }
 
   const handleChange1 = (e: React.FormEvent<HTMLSelectElement>) => {
     setPrintSz(e.currentTarget.value)
+  }
+
+  const handleChange2 = (e: React.FormEvent<HTMLSelectElement>) => {
+    setPrintLocation(e.currentTarget.value)
   }
 
   return (
@@ -87,6 +94,35 @@ const Step2: React.FC = () => {
       <div className={`${styles.step3}`}>
         <div className={`${styles.h1}`}>
           <h1>Bước 3: Chọn máy in</h1>
+        </div>
+        <div className={`${styles.formDiv} ${styles.theForm}`}>
+          <div>
+            <label>
+              Vị trí máy in
+              <br />
+              <select value={printLocation} onChange={handleChange2}>
+                <option value='none' disabled>
+                  Chọn vị trí
+                </option>
+                <option value='ltka4'>LTK - A4.401</option>
+                <option value='ltkc6'>LTK - C6.601</option>
+                <option value='danh6'>Dĩ An - BK.B6</option>
+                <option value='danh2'>Dĩ An - BK.B2</option>
+              </select>
+              <br />
+            </label>
+          </div>
+          <div>
+            <span>Minimap vị trí máy in</span>
+          </div>
+
+          <div className={`${styles.imDiv}`}>
+            <div className={`${styles.img_ltk}`}>
+              <Link to='/print/map1'>
+                <img src={img_map1}></img>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
