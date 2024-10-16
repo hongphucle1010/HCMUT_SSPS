4403355d1b15726145ae39457cc9a99f43a11b65
Le Ngoc Vinh <vinh.lengoc@hcmut.edu.vn>
2024-10-16 10:29:06 +0700

    Add images of map 1 and map 2

--- src/pages/Print/Map1.tsx
+++ src/pages/Print/Map1.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import styles from './Map1.module.scss'
+import img_map1 from '../../assets/map1.png'
 
 const Map1: React.FC = () => {
   return (
     <div>
-      <h1>Map 1</h1>
+      <img src={img_map1} className={`${styles.center}`}></img>
     </div>
   )
 }
--- src/pages/Print/Map2.tsx
+++ src/pages/Print/Map2.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import img_map2 from '../../assets/map2.png'
+import styles from './Map1.module.scss'
 
 const Map2: React.FC = () => {
   return (
     <div>
-      <h1>Map 2</h1>
+      <img src={img_map2} className={`${styles.center}`}></img>
     </div>
   )
 }
