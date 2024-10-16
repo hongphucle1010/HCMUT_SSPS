3c14fdd82f77b88a411256f0898f3c72a775eaba
Phuc Hoang Le <hoanglephuc6@gmail.com>
2024-10-16 16:08:11 +0700

    add NewUpload state

---
+++ src/components/RecentFile/recentfile.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const RecentFile: React.FC = () => {
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        margin: 'auto'
+      }}
+    >
+      <h1>This is recent file</h1>
+    </div>
+  )
+}
+
+export default RecentFile
--- src/pages/Print/StepOne.tsx
+++ src/pages/Print/StepOne.tsx
@@ -1,18 +1,33 @@
 import React from 'react'
 import styles from './StepOne.module.scss'
 import Dropzone from '../../components/Dropzone/Dropzone'
+import RecentFile from '../../components/RecentFile/recentfile'
+import { useState } from 'react'
 
 const Step1: React.FC = () => {
+  const [isInDropzone, setIsInDropzone] = useState(true)
   return (
-    <div className={`${styles.centerBox}`}>
+    <div className={`${styles.centerBox} ${'shadow-2xl'}`}>
       <div className={`${styles.bar}`}>
-        <div className={styles.buttonBar}>
-          <button className={styles.button}>New Upload</button>
-          <button className={styles.button}>Recent</button>
+        <div className={`${styles.buttonBar} ${'rounded-full'}`}>
+          <button
+            className={`${styles.button} ${isInDropzone ? 'bg-black text-white' : 'bg-slate-50 text-black'} ${' focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 dark:bg-gray-800 dark:focus:ring-gray-700'}`}
+            onClick={() => setIsInDropzone(true)}
+          >
+            New Upload
+          </button>
+          <button
+            className={`${styles.button} ${isInDropzone ? 'bg-slate-50 text-black' : 'bg-black text-white'} ${'focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 dark:bg-gray-800 dark:focus:ring-gray-700'}`}
+            onClick={() => setIsInDropzone(false)}
+          >
+            Recent
+          </button>
         </div>
       </div>
-      <Dropzone />
+
+      {isInDropzone ? <Dropzone /> : <RecentFile />}
     </div>
   )
 }
+
 export default Step1
