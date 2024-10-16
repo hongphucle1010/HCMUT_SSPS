e191f4d1d6a21399afeaa6d95f28e7fbc721ff2d
Le Ngoc Vinh <vinh.lengoc@hcmut.edu.vn>
2024-10-16 10:51:22 +0700

    Added button "Quay ve" on map 1 and map 2 pages

--- src/pages/Print/Map1.tsx
+++ src/pages/Print/Map1.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import styles from './Map1.module.scss'
 import img_map1 from '../../assets/map1.png'
+import img_btn from '../../assets/MapButton.png'
+import { Link } from 'react-router-dom'
 
 const Map1: React.FC = () => {
   return (
     <div>
       <img src={img_map1} className={`${styles.center}`}></img>
+      <Link to={'/print/step2'}>
+        <img src={img_btn}></img>
+      </Link>
     </div>
   )
 }
--- src/pages/Print/Map2.tsx
+++ src/pages/Print/Map2.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import img_map2 from '../../assets/map2.png'
 import styles from './Map1.module.scss'
+import img_btn from '../../assets/MapButton.png'
+import { Link } from 'react-router-dom'
 
 const Map2: React.FC = () => {
   return (
     <div>
       <img src={img_map2} className={`${styles.center}`}></img>
+      <Link to={'/print/step2'}>
+        <img src={img_btn}></img>
+      </Link>
     </div>
   )
 }
