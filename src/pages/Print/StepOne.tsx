e53873d9a8b6d81f1618e7783441fb7705702e46
Phuc Hoang Le <hoanglephuc6@gmail.com>
2024-10-16 10:36:22 +0700

    add hcmut logo

--- src/pages/Print/StepOne.tsx
+++ src/pages/Print/StepOne.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import logo from '../../assets/HCMUT_official_logo.png'
+import styles from './StepOne.module.scss'
 
 const Step1: React.FC = () => {
   return (
     <div>
-      <h1>Step 1</h1>
+      <img src={logo} alt='HCMUT' className={&{styles.myImage}}/>
     </div>
   )
 }
