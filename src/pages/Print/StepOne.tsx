f78d26d45e79add3889274663827e18bc2844fbc
Phuc Hoang Le <hoanglephuc6@gmail.com>
2024-10-16 10:44:37 +0700

    minor change

--- src/pages/Print/StepOne.tsx
+++ src/pages/Print/StepOne.tsx
@@ -5,11 +5,7 @@ import styles from './StepOne.module.scss'
 const Step1: React.FC = () => {
   return (
     <div>
-<<<<<<< HEAD
       <img src={logo} alt='logo' className={styles.myImage} />
-=======
-      <h1>Buoc 1</h1>
->>>>>>> main
     </div>
   )
 }
