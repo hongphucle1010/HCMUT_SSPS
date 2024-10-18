ead2deb9f7cbe2f49c53b8e7b3cbe80efec585f8
Phuc Hoang Le <hoanglephuc6@gmail.com>
2024-10-18 23:05:27 +0700

    add file icon

--- src/components/RecentFile/singleFileList.tsx
+++ src/components/RecentFile/singleFileList.tsx
@@ -1,16 +1,33 @@
 import React from 'react'
 import { Table } from 'flowbite-react'
+import { FaRegFilePdf } from 'react-icons/fa6'
+import { TbFileTypeDocx } from 'react-icons/tb'
+import styles from './SingleFileList.module.scss'
+
+function getFileIcon(fileName: string) {
+  const extension = fileName.split('.').pop()
+
+  if (extension === 'pdf') {
+    return <FaRegFilePdf />
+  } else if (extension === 'docx') {
+    return <TbFileTypeDocx />
+  }
+}
 
 const SingleFileList: React.FC<{
   fileName: string
-  fileSize: number
+  fileSize: string
   timeUpload: string
 }> = ({ fileName, fileSize, timeUpload }) => {
   return (
-    <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
-      <Table.Cell className='whitespace-nowrap font-medium text-gray-900 dark:text-white'>{fileName}</Table.Cell>
-      <Table.Cell>{fileSize}</Table.Cell>
-      <Table.Cell>{timeUpload}</Table.Cell>
+    <Table.Row className={`bg-white dark:border-gray-700 dark:bg-gray-800 `}>
+      <Table.Cell>{getFileIcon(fileName)}</Table.Cell>
+      <Table.Cell className={`whitespace-nowrap font-medium text-gray-900 dark:text-white`}>
+        <p>{fileName}</p> <p>{timeUpload}</p>
+      </Table.Cell>
+      <Table.Cell className={`whitespace-nowrap font-medium text-gray-900 dark:text-white`}>
+        <div className={`${styles.box}`}>{fileSize}</div>
+      </Table.Cell>
     </Table.Row>
   )
 }
