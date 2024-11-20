0aaf2f8ca1d3a1139fae2eb71089c0c84ecfbbc2
Hong Phuc Le <72754942+lhplhp10102004@users.noreply.github.com>
2024-11-21 05:16:13 +0700

    update package-lock.json and refactor RecentFile and SingleFileList components for improved readability

--- src/components/RecentFile/singleFileList.tsx
+++ src/components/RecentFile/singleFileList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react'
 import { Table } from 'flowbite-react'
 import { FaRegFilePdf } from 'react-icons/fa6'
@@ -6,13 +7,13 @@ import { useDispatch } from 'react-redux'
 import { setFileReducer } from '../../lib/redux/reducers/printingState'
 import { useState, useEffect } from 'react'
 
-function getFileIcon(fileName: string ) {
+function getFileIcon(fileName: string) {
   const extension = fileName.split('.').pop()
 
   if (extension === 'pdf') {
-    return <FaRegFilePdf size={20}/>
+    return <FaRegFilePdf size={20} />
   } else if (extension === 'docx') {
-    return <TbFileTypeDocx size={20}/>
+    return <TbFileTypeDocx size={20} />
   }
 }
 
@@ -21,51 +22,47 @@ const SingleFileList: React.FC<{
   updatedAt: string
 }> = ({ fileName, updatedAt }) => {
   const UpdatedAt = new Date(updatedAt)
-  const dispatch = useDispatch();
-  
-  const [currentTime, setCurrentTime] = useState(new Date());
-  const [timeElapsed, setTimeElapsed] = useState('');
+  const dispatch = useDispatch()
 
-  useEffect(
-    () => {
-      const intervalId = setInterval(() => {
-        setCurrentTime(new Date());
-      }, 1000);
+  const [currentTime, setCurrentTime] = useState(new Date())
+  const [timeElapsed, setTimeElapsed] = useState('')
 
-      return () => {
-        clearInterval(intervalId)
-      }
-    }, [currentTime]
-  );
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date())
+    }, 1000)
 
-  useEffect(
-    () => {
-      console.log(typeof UpdatedAt)
-      const timeDiff = currentTime.getTime() - UpdatedAt.getTime();
-      const seconds = Math.floor(timeDiff / 1000);
-      const minutes = Math.floor(seconds / 60);
-      const hours   = Math.floor(minutes / 60);
-      const days    = Math.floor(hours / 24);
-      if (days > 0){
-        setTimeElapsed(`${days} days ago`);
-      }
-      else if (hours > 0){
-        setTimeElapsed(`${hours} hours ago`);
-      }
-      else if (minutes > 0){
-        setTimeElapsed(`${minutes} minutes ago`);
-      }
-      else{
-        setTimeElapsed(`${seconds} seconds ago`)
-      }
-    }, [currentTime]
-  );  
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [currentTime])
+
+  useEffect(() => {
+    console.log(typeof UpdatedAt)
+    const timeDiff = currentTime.getTime() - UpdatedAt.getTime()
+    const seconds = Math.floor(timeDiff / 1000)
+    const minutes = Math.floor(seconds / 60)
+    const hours = Math.floor(minutes / 60)
+    const days = Math.floor(hours / 24)
+    if (days > 0) {
+      setTimeElapsed(`${days} days ago`)
+    } else if (hours > 0) {
+      setTimeElapsed(`${hours} hours ago`)
+    } else if (minutes > 0) {
+      setTimeElapsed(`${minutes} minutes ago`)
+    } else {
+      setTimeElapsed(`${seconds} seconds ago`)
+    }
+  }, [currentTime])
 
   return (
-    <Table.Row className={`bg-white dark:border-gray-700 dark:bg-gray-800 cursor-pointer`} onClick={() => dispatch(setFileReducer(fileName))}>
+    <Table.Row
+      className={`bg-white dark:border-gray-700 dark:bg-gray-800 cursor-pointer`}
+      onClick={() => dispatch(setFileReducer(fileName))}
+    >
       <Table.Cell className={`w-1`}>{getFileIcon(fileName)}</Table.Cell>
       <Table.Cell className={`whitespace-nowrap font-medium text-gray-900 dark:text-white`}>
-        <p style={{fontWeight: 'bold'}}>{fileName}</p> <p>{timeElapsed}</p>
+        <p style={{ fontWeight: 'bold' }}>{fileName}</p> <p>{timeElapsed}</p>
       </Table.Cell>
     </Table.Row>
   )
